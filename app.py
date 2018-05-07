8decae6e51a15636dc133966b13d1aeb3e7e445a
Dennis Tran <dennistran714@gmail.com>
2018-05-07 16:41:51 -0700

    updated dash app file

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 # Initialize Dash app (similar to Flask)
-app = Dash()
+app = Dash(__name__)
+server = app.server
 
 ###################################
 #                                 #
